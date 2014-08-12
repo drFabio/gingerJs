317ade0930df070aba1906239370965e5cbd940a
Fabio Oliveira Costa <blackjackdevel@gmail.com>
2014-08-12 19:29:25 +0000

    Made all crud responses use the same method

--- core/mvc/AbstractCRUDController.js
+++ core/mvc/AbstractCRUDController.js
@@ -3,7 +3,12 @@ module.exports= {
 
 	createAction:function(req,res){
 		var data=req.query.data;
+		var self=this;
 		this._model.create(data,function(err,data){
+			self._sendResponse(req,res,err,data);
+		});
+	},
+	_sendResponse:function(req,res,err,data){
 			var response;
 			if(err){
 				res.status(200).send(err);
@@ -13,51 +18,28 @@ module.exports= {
 				response=JSON.stringify(data);
 			}
 			res.status(200).send(response);
-		});
 	},
 	updateAction:function(req,res){
 		var search=req.query.search;
 		var data=req.query.data;
+		var self=this;
 		this._model.update(data,search,function(err,data){
-			var response;
-			if(err){
-				res.status(200).send(err);
-				return;
-			}
-			else{
-				response=JSON.stringify(data);
-			}
-			res.status(200).send(response);
+			self._sendResponse(req,res,err,data);
 		});
 	},
 	readAction:function(req,res){
 		var search=req.query.search;
+		var self=this;
 		this._model.read(search,function(err,data){
-			var response;
-			if(err){
-				res.status(200).send(err);
-				return;
-
-			}
-			else{
-				response=JSON.stringify(data);
-			}
-			res.status(200).send(response);
+			self._sendResponse(req,res,err,data);
 		});
 
 	},
 	destroyAction:function(req,res){
 		var search=req.query.search;
+		var self=this;
 		this._model.destroy(search,function(err,data){
-			var response;
-			if(err){
-				res.status(200).send(err);
-				return;
-			}
-			else{
-				response=JSON.stringify(data);
-			}
-			res.status(200).send(response);
+			self._sendResponse(req,res,err,data);
 		});
 	}
 	
