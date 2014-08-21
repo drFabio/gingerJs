4abee9aeec3f65ff27b4ef591d970788abc38afc
Fabio Oliveira Costa <blackjackdevel@gmail.com>
2014-08-21 23:12:45 +0000

    Fix

--- core/gateways/JSONRPC.js
+++ core/gateways/JSONRPC.js
@@ -70,6 +70,9 @@ module.exports = {
                 }
                 else{
                     result= body;
+                    if(!result){
+                        result=true;
+                    }
 
                 }
             }
