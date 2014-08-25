8f98a753342d3ecde8f43f7acb32970e326c999c
Fabio Oliveira Costa <blackjackdevel@gmail.com>
2014-08-25 23:42:12 +0000

    Update tests implemented

--- core/tests/core/components/DataBase.js
+++ core/tests/core/components/DataBase.js
@@ -9,7 +9,7 @@ var should = utils.should;
 var fixtures=utils.fixtures;
 var fixtureData;
 
-describe.only('Component database',function(){
+describe('Component database',function(){
 	var ginger;
 	//Initializing the app by path first
 	before(function(done){
@@ -26,85 +26,177 @@ describe.only('Component database',function(){
 		}
 		utils.initServer(cb);
 	});
-	beforeEach(function(done){
-		fixtures.clearAndLoad(fixtureData,done);
-	});
-	describe('readById',function(){
-		describe('Failure',function(){
-			it('Should return not found if the id is not valid ',function(done){
-				databaseComponent.readById('login','NotRelevant',function(err,data){
-					expect(err).to.exist;
-					expect(err.code).to.equal('NOT_FOUND');
-					done();
+
+	describe('read',function(){
+		before(function(done){
+			fixtures.clearAndLoad(fixtureData,done);
+		});
+		describe('By id',function(){
+			describe('Failure',function(){
+				it('Should return invalid params if the id is not valid ',function(done){
+					databaseComponent.readById('login','NotRelevant',function(err,data){
+						expect(err).to.exist;
+						expect(err.code).to.equal('INVALID_PARAMS');
+						done();
+					});
+				});
+				it('Should return not found if the id is not found ',function(done){
+					databaseComponent.readById('login',createObjectId(),function(err,data){
+						expect(err).to.exist;
+						expect(err.code).to.equal('NOT_FOUND');
+						done();
+					});
 				});
 			});
-			it('Should return not found if the id is not found ',function(done){
-				databaseComponent.readById('login',createObjectId(),function(err,data){
-					expect(err).to.exist;
-					expect(err.code).to.equal('NOT_FOUND');
-					done();
+			describe('success',function(){
+				it('Should find the element by id successfuly',function(done){
+					var id=fixtureData.login.user1._id.toString();
+					databaseComponent.readById('login',id,function(err,data){
+					
+						expect(data._id.toString()).to.equal(id);
+						done();
+					});
+				});
+				it('Should be able to restrict the fields',function(done){
+					var id=fixtureData.login.user1._id.toString();
+					var cb=function(err,data){
+					
+						expect(data._id.toString()).to.equal(id);
+						var expectKeys=['email','_id'];
+						var keys=Object.keys(data);
+						var intersection=_.intersection(keys,expectKeys);
+						var sizeResponse=keys.length;
+						var intersectionSize=intersection.length;
+						expect(intersectionSize).to.equal(sizeResponse);
+						done();
+					};
+					var fields='email';
+					databaseComponent.readById('login',id,cb,fields);
 				});
 			});
 		});
-		describe('success',function(){
-			it('Should find the element by id successfuly',function(done){
-				var id=fixtureData.login.user1._id.toString();
-				databaseComponent.readById('login',id,function(err,data){
-				
-					expect(data._id.toString()).to.equal(id);
-					done();
-				});
-			});
-			it('Should be able to restrict the fields',function(done){
-				var id=fixtureData.login.user1._id.toString();
-				var cb=function(err,data){
-				
-					expect(data._id.toString()).to.equal(id);
-					var expectKeys=['email','_id'];
-					var keys=Object.keys(data);
-					var intersection=_.intersection(keys,expectKeys);
-					var sizeResponse=keys.length;
-					var intersectionSize=intersection.length;
-					expect(intersectionSize).to.equal(sizeResponse);
-					done();
-				};
-				var fields='email';
-				databaseComponent.readById('login',id,cb,fields);
-			});
+		describe('One',function(){
 		});
-	});
-	describe('Read',function(){
-	});
-	describe('Read one',function(){
+		describe(' normal ',function(){
+		});
 	});
 	describe('Create',function(){
+		beforeEach(function(done){
+			fixtures.clearAndLoad(fixtureData,done);
+		});
 		describe('Failure',function(){
 			describe('Validation',function(){
 			});
 			it('Should not be able to create  without required fields');
 		});
-		describe('Success',function(){
-			it('Should successfuly create',function(done){
-				var data={
-					'email':'bar@baz.com',
-					'active':true,
-					'name':'barBaz',
-					'password':'foo',
+		it('Should execute successfuly ',function(done){
+			var data={
+				'email':'bar@baz.com',
+				'active':true,
+				'name':'barBaz',
+				'password':'foo',
+			}
+			var cb=function(err,saveData){
+				expect(saveData).to.exist;
+				for(var k in data){
+					expect(data[k]).to.equal(saveData[k])
 				}
-				var cb=function(err,saveData){
-					expect(saveData).to.exist;
-					for(var k in data){
-						expect(data[k]).to.equal(saveData[k])
-					}
-					done();
-				}
-				databaseComponent.create('login',data,cb);
-			});
+				done();
+			}
+			databaseComponent.create('login',data,cb);
 		});
 	});
 	describe('Update',function(){
+		describe('failure',function(){
+			it('Should return 0 if no item was updated',function(done){
+				var newEmail='newEmail@email.com';
+				var notFoundEmail='notFound@notFound.com';
+				var cb=function(err,numAffected){
+					expect(err).to.not.exist;
+					expect(numAffected).to.equal(0);
+					done();
+				}
+				databaseComponent.update('login',{'email':newEmail},{'email':notFoundEmail},cb);
+			});
+		});
+		describe('success',function(){
+			beforeEach(function(done){
+				fixtures.clearAndLoad(fixtureData,done);
+			});
+			it('Should happen by mixed params',function(done){
+				var email=fixtureData.login.user1.email;
+				var newEmail='newEmail@email.com';
+				var name=fixtureData.login.user1.name;
+				var cb=function(err,data){
+					expect(data).to.equal(1);
+					done();
+				}
+				databaseComponent.update('login',{'email':newEmail},{'email':email,'name':name},cb);
+			});
+			it('Should update multiple instances if they all match',function(done){
+				var name=fixtureData.login.user1.name;
+				var newName='new name';
+				var cb=function(err,data){
+					expect(data).to.equal(2);
+					done();
+				}
+				databaseComponent.update('login',{'name':newName},{'name':name},cb);
+			});
+		});
+		describe('By id',function(){
+			beforeEach(function(done){
+				fixtures.clearAndLoad(fixtureData,done);
+			});
+			it('Should execute successfuly',function(done){
+				var id=fixtureData.login.user1._id.toString();
+				var newName='new name';
+				var cb=function(err,data){
+					expect(data).to.exist
+					expect(data._id.toString()).to.equal(id);
+					expect(data.name).to.equal(newName);
+					
+					done();
+				}
+				databaseComponent.updateById('login',id,{'name':newName},cb);
+			});
+			it('Should return InvalidParams if the id is not valid',function(done){
+				var id='InvalidId';
+				var newName='new name';
+				var cb=function(err,data){
+			
+					expect(err).to.exist;
+					expect(err.code).to.equal('INVALID_PARAMS');
+					
+					done();
+				}
+				databaseComponent.updateById('login',id,{'name':newName},cb);
+
+			});
+			it('Should return not found if the id is not found',function(done){
+				var id=createObjectId();
+				var newName='new name';
+				var cb=function(err,data){
+					expect(err).to.exist;
+					expect(err.code).to.equal('NOT_FOUND');
+					
+					done();
+				}
+				databaseComponent.updateById('login',id,{'name':newName},cb);
+			});
+
+		});
+	});
+	describe('Save',function(){
+	});
+	describe('List',function(){
 	});
 	describe('Destroy ',function(){
+		it('Should to delete a component by params');
+		describe('byId',function(){
+			it('Should execute successfuly');
+			it('Should return not found if the id is not valid');
+			it('Should return not found if the id is not found');
+		});
 	});
 
 	after(function(done){
--- core/tests/fixtures/login.js
+++ core/tests/fixtures/login.js
@@ -2,7 +2,15 @@ var id = require('pow-mongodb-fixtures').createObjectId;
 module.exports= {
 	'login':{
 		'user1':{
-			'email':'blackjackdevel@gmail.com',
+			'email':'email@gmail.com',
+			'active':true,
+			'name':'bar',
+			'password':'foo',
+			'_id':id()
+
+		},
+		'user2':{
+			'email':'othermail@gmail.com',
 			'active':true,
 			'name':'bar',
 			'password':'foo',
