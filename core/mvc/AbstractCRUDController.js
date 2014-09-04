a114e15cd61fd86207b74216d3b0755bf005857a
Fabio Oliveira Costa <blackjackdevel@gmail.com>
2014-09-04 03:52:30 +0000

    Fix typo

--- core/mvc/AbstractCRUDController.js
+++ core/mvc/AbstractCRUDController.js
@@ -2,36 +2,30 @@ module.exports= {
 	parent:'ginger.mvc.AbstractController',
 	saveAction:function(req,res){
 		var data=req.query.data;
-		var self=this;
 		this._model.save(data,this._getSendResponse(req,res));
 	},
 	createAction:function(req,res){
 		var data=req.query.data;
-		var self=this;
 		this._model.create(data,this._getSendResponse(req,res));
 	},
 	updateAction:function(req,res){
 		var search=req.query.search;
 		var data=req.query.data;
-		var self=this;
 		this._model.update(data,search,this._getSendResponse(req,res));
 	},
-	updateById:function(req,res){
+	updateByIdAction:function(req,res){
 		var id=req.query.id;
 		var data=req.query.data;
-		var self=this;
 		this._model.updateById(id,data,this._getSendResponse(req,res));
 	},
 	readAction:function(req,res){
 		var search=req.query.search;
 		var fields=req.query.fields;
-		var self=this;
 		this._model.read(search,this._getSendResponse(req,res),fields);
 
 	},
 	destroyAction:function(req,res){
 		var search=req.query.search;
-		var self=this;
 		this._model.destroy(search,this._getSendResponse(req,res));
 	},
 	readByIdAction:function(req,res){
