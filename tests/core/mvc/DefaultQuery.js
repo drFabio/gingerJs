8e1def509fe10fe7fd843f99e7fa9ec1ad7b9500
Fabio <blackjackdeve@gmail.com>
2014-10-01 03:37:45 +0000

    Tests for query on fields implemented

--- tests/core/mvc/DefaultQuery.js
+++ tests/core/mvc/DefaultQuery.js
@@ -10,7 +10,7 @@ var fixtures=utils.fixtures;
 var	dbFixtureData=utils.getFixtureData('categories','login');
 var ginger;
 var queryFactory;
-describe('DefaultQuery',function(){
+describe.only('DefaultQuery',function(){
 	before(function(done){
 		utils.initServer(function(err){
 			if(err){
@@ -21,6 +21,7 @@ describe('DefaultQuery',function(){
 			done();
 		});
 	});
+
 	it('Should be able to set the normal options ',function(done){
 		var search={'fieldA':'equal comething'};
 		var field={'fieldB':true,'fieldC':true};
@@ -33,11 +34,45 @@ describe('DefaultQuery',function(){
 		expect(_.isEqual(query._option,option)).to.be.true;
 		for(var k in populate){
 			expect(query._populate[k].path).to.equal(k);
-			expect(query._populate[k].select).to.equal(populate[k]);
+			var populateParts=query._getSelectPartFromString(populate[k]);
+			expect(_.isEqual(query._populate[k].select,populateParts)).to.equal(true);
 		}
 		done();
 	});
 	describe('Population',function(){
+		it('Should be able to set population from a field,while respecting both field and population',function(done){
+			var field={
+				'populated.someField':{'foo':true,'bar':true,'baz':true},
+				'normalField':'bar'
+			}
+			var populate={
+				'anotherPopulated':'name _id enabled',
+				'populated':'someOtherField'
+				
+			}
+			var query=ginger.getQuery(null,field,null,populate);
+			console.log(query._populate);
+			for(var k in field){
+				if(k.indexOf('.')>-1){
+					var parts=k.split('.');
+					expect(query._populate[parts[0]]).to.exist;
+					expect(query._populate[parts[0]].select[parts[1]]).to.equal(field[k]);
+				}
+				else{
+
+					expect(query._field[k]).to.equal(field[k]);
+				}
+			}
+			for(var k in populate){
+				expect(query._populate[k].path).to.equal(k);
+				var populateParts=query._getSelectPartFromString(populate[k]);
+				for(var populateKey in populateParts){
+
+					expect(query._populate[k].select[populateKey]).to.equal(populateParts[populateKey]);
+				}
+			}
+			done();
+		});
 	});
 	after(function(done){
 		utils.endServer(done);
