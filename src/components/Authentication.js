baa27281711344f0031f330316ccfb9de38f1176
Fabio <blackjackdevel@gmail.com>
2014-11-13 21:02:02 +0000

    Fixing component authenticator

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -9,12 +9,16 @@ module.exports={
 		var expressComponent=this._engine.getComponent('express');
 		var app=expressComponent.getApp();	
 		app.use(passport.initialize());
+		this._passport=passport;
 		app.use(passport.session()); // persistent login sessions
 /*
 		if(!engine.hasModel(this._params.model)){
 			throw new Error('The authentication component needs the '+this._params.model+' to work (you can change the name of the model trough the config)');
 		}
 		this._model=engine.getModel(this._params.model);*/
+	},
+	getPassport:function(){
+		return this._passport;
 	},
 	setLoginStrategy:function(){
 		/*var LocalStrategy   = require('passport-local').Strategy;
